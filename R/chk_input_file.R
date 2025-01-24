b5dcb9ac7637d998adae27d8aa4477c5fafde1db
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-01-24 17:52:01 +1300

    good to go

---
+++ R/chk_input_file.R
@@ -0,0 +1,24 @@
+#' @importFrom cli cli_abort
+#' @importFrom tools file_ext
+#'
+#' @keywords internal
+#' @noRd
+.check_input_file <- function(file,
+                              ext,
+                              call) {
+  file <- path.expand(path = file)
+  if (missing(x = file)) {
+    cli::cli_abort(c("x" = "{.arg {rlang::caller_arg(file)}} has not been provided."),
+                   call = call)
+  } else if (!file.exists(file)) {
+    cli::cli_abort(c("x" = "Cannot open file {.file {file}}: No such file."),
+                   call = call)
+  } else {
+    file_ext <- tools::file_ext(x = basename(path = file))
+    if (!identical(x = tolower(x = file_ext), y = ext)) {
+      cli::cli_abort(c("x" = "{.arg {rlang::caller_arg(file)}} must be a {ext} file, not {file_ext} file."),
+                     call = call)
+    }
+  }
+  return(file)
+}
