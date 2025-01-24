566bd5d6fc295d1da92b93fc61b3afc66012bd78
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-01-24 17:46:42 +1300

    good to go

---
+++ R/chk_time_steps.R
@@ -0,0 +1,54 @@
+.check_time_steps <- function(CPHI,
+                              LRORG,
+                              KAPPA,
+                              interval_switch,
+                              time_steps,
+                              call) {
+  if (!interval_switch) {
+    t0 <- time_steps[1]
+    time_steps <- diff(x = time_steps)
+  } else {
+    t0 <- NA
+  }
+
+  n_timestep <- as.numeric(length(x = time_steps) + 1)
+  if (grepl(pattern = "\\.csv", x = CPHI)) {
+    CPHI <- .check_input_file(file = CPHI,
+                              ext = "csv",
+                              call = call)
+
+    .check_time_validity(file = CPHI,
+                         col = c("REGr", "ALLTIMEt", "Value"),
+                         call = call,
+                         n_timestep = n_timestep)
+  }
+
+  if (grepl(pattern = "\\.csv", x = KAPPA)) {
+    KAPPA <- .check_input_file(file = KAPPA,
+                               ext = "csv",
+                               call = call)
+
+    .check_time_validity(file = KAPPA,
+                         col = c("REGr", "ALLTIMEt", "Value"),
+                         call = call,
+                         n_timestep = n_timestep)
+  }
+
+  if (grepl(pattern = "\\.csv", x = LRORG)) {
+    LRORG <- .check_input_file(file = LRORG,
+                               ext = "csv",
+                               call = call)
+
+    .check_time_validity(file = LRORG,
+                         col = c("ALLTIMEt", "Value"),
+                         call = call,
+                         n_timestep = n_timestep)
+  }
+
+ls_int <- list(t0 = t0,
+               CPHI = CPHI,
+               KAPPA = KAPPA,
+               LRORG = LRORG)
+
+return(ls_int)
+}
